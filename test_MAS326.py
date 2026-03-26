f3f67f5c4c8f069b708133446fd769c8249d265c
hyunwook990 <jho99061288@gmail.com>
2026-03-26 11:17:51 +0900

    instruction 수정 후 재출력

--- test_MAS326.py
+++ test_MAS326.py
@@ -1,4 +1,4 @@
-# code template으로 논문에 나온 코드 제공
+# code template으로 논문에 나온 코드를 제공해서 출력
 # generated token count: 725
 # To solve the user query, we need to implement the `call_llm` function and the `LLM` class. We also need to implement the `get_permutations` function to generate all permutations of a given list.
 
@@ -53,3 +53,50 @@ print(mas.forward(query))
 ###################################################################
 ###################################################################
 ###################################################################
+
+# github에 있던 for_instruction에 논문에 나온 code 예시를 code example로 추가해서 출력
+# generated token count: 519
+# To solve the user query, we need to implement the `forward` method in the `MAS` class. Here's the updated code:
+
+from utils import *
+
+class MAS():
+    def __init__(self, model_list):
+        self.llm = LLM(model_list)
+
+    def forward(self, query):
+        # Parse the query to extract the permutation and the set
+        from sympy import Permutation, S
+        import re
+        match = re.match(r'Let p = \((\d+), (\d+), (\d+), (\d+)\)\((\d+), (\d+)\) in S_(\d+)', query)
+        if match:
+            p = Permutation([int(x) for x in match.group(1).split(', ')], [int(x) for x in match.group(2).split(', ')])
+            n = int(match.group(5))
+            S = S(n)
+        else:
+            raise ValueError("Invalid query")
+
+        # Check if the permutation is in the set
+        if p in S:
+            index = list(S).index(p)
+        else:
+            index = -1  # or raise an exception
+
+        # Generate the final answer
+        answer_agent = f'The index of <p> in S_{n} is {index}'
+        return call_llm(answer_agent)
+
+# This code uses the `sympy` library to parse the permutation and the set, and then checks if the permutation is in the set. If it is, it returns the index of the permutation in the set. If not, it returns -1 (or raises an exception).
+
+# Note that this code assumes that the query is in the correct format, i.e., "Let p = (1, 2, 5, 4)(2, 3) in S_5". If the query is not in this format, the code will raise a `ValueError`.
+
+# Also, this code uses the `call_llm` function from the `utils` library to call the large language model. This function is not defined in the code snippet, so you will need to implement it yourself.
+
+# You can use this code as follows:
+
+mas = MAS(['model1','model2'])
+query = 'Let p = (1, 2, 5, 4)(2, 3) in S_5'
+answer = mas.forward(query)
+print(answer)
+
+# This will output the index of the permutation in the set.
